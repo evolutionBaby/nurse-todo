736f1446b229a4ef7a9ddcaf7a3a37717c96a7a2
Piotr Knopczyński <piotr.knopczynski@appunite.com>
2018-08-17 10:55:49 +0200

    vuex, sass loaders setted up

---
+++ src/components/NurseTodo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>Nurse Visits</div>
+</template>
+
+<script>
+export default {
+  name: 'NurseTodo'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+$green: #42b983;
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: $green;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App'
+import Vuex from 'vuex'
+import storeModules from './store'
+
+Vue.use(Vuex)
+const store = new Vuex.Store(storeModules)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store: store,
   render: h => h(App)
 })
